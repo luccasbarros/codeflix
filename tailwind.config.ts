28f7de9d327d42040e30088d43f97fd9e4530ee6
Luccas Barros <luccas.barros@magicmedia.studio>
2025-03-07 07:30:01 -0300

    chore: deps and tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+import tailwindScrollbarHide from 'tailwind-scrollbar-hide';
 
 export default {
   content: [
@@ -14,5 +15,7 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    tailwindScrollbarHide
+  ],
 } satisfies Config;
